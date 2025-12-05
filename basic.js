74f07739c4b1862796c688987c0159f1ed53a769
Pathivada Greeshma <23A91A05I0@aec.edu.in>
2025-12-05 11:47:23 +0530

    first commit

---
+++ basic.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send("Hello this is from backend");
+})
+
+app.get('/get-users',(req,res)=>{
+    res.send("hello this is from frontend");
+})
+app.listen(7005, ()=>{
+    console.log("server running at port 7005")
+});
